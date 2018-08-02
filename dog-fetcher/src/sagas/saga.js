8a878e6c51c9d5f05b0817f170c60e34de378b26
onur <onurcskunimp@gmail.com>
2018-08-02 12:06:24 +0300

    add axios, fetched with axios

--- dog-fetcher/src/sagas/saga.js
+++ dog-fetcher/src/sagas/saga.js
@@ -1,19 +1,17 @@
 import {takeEvery} from 'redux-saga'
-import {put, call} from 'redux-saga/effects'
+import {put} from 'redux-saga/effects'
 import { dogRequest, dogRequestSuccess, dogRequestFailure } from '../actions/actions';
+import axios from 'axios'
 
 export function* watchFetchDog() {
     yield takeEvery('FETCHED_DOG', fetchDogAsync);
 }
-  
+
 export function* fetchDogAsync() {
     try {
         yield put(dogRequest());
-        const data = yield call(() => {
-            return fetch('https://dog.ceo/api/breeds/image/random')
-                    .then(res => res.json())
-        });
-        yield put(dogRequestSuccess(data));
+        const res = yield axios.get('https://dog.ceo/api/breeds/image/random')
+        yield put(dogRequestSuccess(res.data));
     } catch (error) {
         yield put(dogRequestFailure());
     }
